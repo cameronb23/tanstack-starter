c4703321b29eda748b3d21c24abf360803c4520b
Cameron Butler <7783071+cameronb23@users.noreply.github.com>
2024-11-02 23:43:47 -0400

    feat: i18n via i18next (#2)

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,11 +1,17 @@
 import type { ReactNode } from 'react';
-import { createRootRouteWithContext } from '@tanstack/react-router';
+import {
+  createRootRouteWithContext,
+  useRouteContext,
+} from '@tanstack/react-router';
 import { Outlet, ScrollRestoration } from '@tanstack/react-router';
 import { Body, Head, Html, Meta, Scripts } from '@tanstack/start';
 import { ThemeProvider } from '@/components/theme/ThemeProvider';
 import type { QueryClient } from '@tanstack/react-query';
 import { getAuthQueryOptions } from '@/services/auth';
+import { I18nextProvider } from 'react-i18next';
+import i18n, { getLocale } from '@/lib/i18n';
 
+import '@/lib/i18n';
 import globalCss from '@/styles/globals.css?url';
 import geist from 'non.geist?url';
 import geistMono from 'non.geist/mono?url';
@@ -48,7 +54,10 @@ window.$RefreshSig$ = () => (type) => type`,
       getAuthQueryOptions(),
     );
 
-    return { auth };
+    // You could also get geolocation for the locale
+    const locale = await getLocale();
+
+    return { auth, locale };
   },
 });
 
@@ -61,17 +70,21 @@ function RootComponent() {
 }
 
 function RootDocument({ children }: { children: ReactNode }) {
+  const { locale } = useRouteContext({ from: '__root__' });
+
   return (
     <Html>
       <Head>
         <Meta />
       </Head>
       <Body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
-        <ScrollRestoration />
-        <Scripts />
+        <I18nextProvider i18n={i18n.cloneInstance({ lng: locale })}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+          <ScrollRestoration />
+          <Scripts />
+        </I18nextProvider>
       </Body>
     </Html>
   );
